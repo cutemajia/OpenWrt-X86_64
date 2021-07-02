74e034ce4a47eb3b0c3247de89503af7ec7213e8
cutemajia <85051114+cutemajia@users.noreply.github.com>
2021-07-02 12:32:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,6 @@
 sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git oaf https://github.com/destan19/OpenAppFilter' feeds.conf.default
-sed -i '$a src-git argon https://github.com/jerrykuku/luci-theme-argon.git;18.06' feeds.conf.default
+#sed -i '$a src-git argon https://github.com/jerrykuku/luci-theme-argon.git;18.06' feeds.conf.default
+cd openwrt/package
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
